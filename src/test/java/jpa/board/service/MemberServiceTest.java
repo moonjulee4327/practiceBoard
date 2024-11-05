363a3b949452ac561e98960854d9741c56e19000
moonju lee <mjlee4327@gmail.com>
2024-11-06 02:42:06 +0900

    test: SpringBootTest 이용한 테스트

--- src/test/java/jpa/board/service/MemberServiceTest.java
+++ src/test/java/jpa/board/service/MemberServiceTest.java
@@ -7,19 +7,17 @@ import jpa.board.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class MemberServiceTest {
+    @Autowired
     private MemberService memberService;
-
-    @BeforeEach
-    void beforeEach() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(BoardApplication.class);
-        memberService = ac.getBean("memberService", MemberService.class);
-    }
 
     @Test
     @DisplayName("회원가입")
