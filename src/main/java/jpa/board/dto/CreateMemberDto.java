95060a598d73a4cf214e6d5b6451502599411908
moonju lee <mjlee4327@gmail.com>
2024-11-20 23:57:55 +0900

    fix: 기본 생성자 추가

--- src/main/java/jpa/board/dto/CreateMemberDto.java
+++ src/main/java/jpa/board/dto/CreateMemberDto.java
@@ -2,9 +2,11 @@ package jpa.board.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateMemberDto {
     private String name;
 
