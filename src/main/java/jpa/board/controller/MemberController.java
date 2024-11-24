76bed3102f03f87a4d5f76fda8ea4496800286ef
moonju lee <mjlee4327@gmail.com>
2024-11-24 20:37:20 +0900

    refactor: url 수정 및 메소드 명 변경

--- src/main/java/jpa/board/controller/MemberController.java
+++ src/main/java/jpa/board/controller/MemberController.java
@@ -13,37 +13,38 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 @RestController
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
-    @PostMapping("/members")
-    public ResponseEntity<CreateMemberResponseDto> saveMember(@RequestBody CreateMemberDto createMemberDto) {
+    @PostMapping("")
+    public ResponseEntity<CreateMemberResponseDto> createMember(@RequestBody CreateMemberDto createMemberDto) {
         Long saveMemberNo = memberService.join(createMemberDto);
         return ResponseEntity.ok(new CreateMemberResponseDto(saveMemberNo));
     }
 
-    @GetMapping("/members")
-    public ResponseEntity<Object> listMember() {
+    @GetMapping("")
+    public ResponseEntity<Object> getMembers() {
         List<MemberDto> list = memberService.findAll();
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping("/members/{memberId}")
-    public ResponseEntity<MemberDto> findOneMember(@PathVariable("memberId") Long memberId) {
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberDto> getMember(@PathVariable("memberId") Long memberId) {
         MemberDto memberDto = memberService.findOneMember(memberId);
         return ResponseEntity.ok(memberDto);
     }
 
-    @PatchMapping("/members/{memberId}")
-    public ResponseEntity<UpdateMemberResponseDto> updateNickname(@PathVariable("memberId") Long memberId,
+    @PatchMapping("/{memberId}")
+    public ResponseEntity<UpdateMemberResponseDto> updateMember(@PathVariable("memberId") Long memberId,
                                                                 @RequestBody UpdateNicknameDto updateNicknameDto) {
         Long memberNo = memberService.updateNickname(memberId, updateNicknameDto.getNickname());
         return ResponseEntity.ok(new UpdateMemberResponseDto(memberNo));
     }
 
-    @DeleteMapping("/members/{memberId}")
-    public ResponseEntity<Long> deleteOneMember(@PathVariable("memberId") Long memberId) {
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<Long> deleteMember(@PathVariable("memberId") Long memberId) {
         memberService.deleteMember(memberId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
