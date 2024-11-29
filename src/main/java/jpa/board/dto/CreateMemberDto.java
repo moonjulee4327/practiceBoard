caa443ee876b09cab32671031a759609f8c206ba
moonju lee <mjlee4327@gmail.com>
2024-11-29 22:51:55 +0900

    feat: 회원 이메일 추가

--- src/main/java/jpa/board/dto/CreateMemberDto.java
+++ src/main/java/jpa/board/dto/CreateMemberDto.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateMemberDto {
+    private String email;
+
     private String name;
 
     private String password;
