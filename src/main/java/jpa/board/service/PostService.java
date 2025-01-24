d73827e473db1a388ef3670e900210573db8d1ee
moonju lee <mjlee4327@gmail.com>
2025-01-24 23:34:03 +0900

    refactor: 로그 추가 및 예외 처리 수정

--- src/main/java/jpa/board/service/PostService.java
+++ src/main/java/jpa/board/service/PostService.java
@@ -6,17 +6,20 @@ import jpa.board.dto.PostDto;
 import jpa.board.exception.PostNotFoundException;
 import jpa.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final MemberService memberService;
 
+    @Transactional
     public PostDto.Response savePost(PostDto.Request request) {
         Member member = memberService.findAuthenticatedMember();
 
@@ -26,6 +29,7 @@ public class PostService {
                 .member(member)
                 .build();
         postRepository.save(post);
+        log.info("Added Post ID : {}", post.getId());
 
         return new PostDto.Response(post);
     }
@@ -42,21 +46,24 @@ public class PostService {
     public PostDto.Response findOnePost(Long postId) {
         return  postRepository.findById(postId)
                 .map(PostDto.Response::new)
-                .orElseThrow(() -> new PostNotFoundException("Post ID : " + postId + " Not Found", postId));
+                .orElseThrow(() -> new PostNotFoundException("Post ID : " + postId + " Not Found (Post One)", postId));
     }
 
     @Transactional
     public PostDto.Response updateOnePost(Long postId, PostDto.Request request) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new PostNotFoundException("Post ID : " + postId + " Not Found", postId));
+                .orElseThrow(() -> new PostNotFoundException("Post ID : " + postId + " Not Found (Update Post)", postId));
         Long updatePostId = post.updatePost(request.getTitle(), request.getContent());
+        log.info("Updated Post ID : {}", postId);
         return new PostDto.Response(updatePostId);
     }
 
+    @Transactional
     public void deletePostById(Long postId) {
         if(!postRepository.existsById(postId)) {
-            throw new PostNotFoundException("Post ID : " + postId + " Not Found", postId);
+            throw new PostNotFoundException("Post ID : " + postId + " Not Found (Delete Post)", postId);
         }
         postRepository.deleteById(postId);
+        log.info("Deleted Post ID : {}", postId);
     }
 }
