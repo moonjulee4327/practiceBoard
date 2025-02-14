c461a57de6b0099ed15ccbc0f71c2931cc1ad7d2
moonju lee <mjlee4327@gmail.com>
2025-02-14 22:47:12 +0900

    test: 댓글 저장 테스트 코드 추가

--- src/test/java/jpa/board/service/CommentServiceTest.java
+++ src/test/java/jpa/board/service/CommentServiceTest.java
@@ -4,18 +4,23 @@ import jpa.board.domain.Comment;
 import jpa.board.domain.Member;
 import jpa.board.domain.Post;
 import jpa.board.domain.RoleType;
+import jpa.board.dto.CommentDto;
+import jpa.board.dto.MemberDto;
 import jpa.board.repository.CommentRepository;
 import jpa.board.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
@@ -28,14 +33,28 @@ class CommentServiceTest {
     @Mock
     private MemberService memberService;
 
-    @Mock
-    private SecurityContextService securityContextService;
-
     @InjectMocks
     private CommentService commentService;
 
     @Test
     void addCommentToPost() {
+        Member mockMember = createMember();
+        Post post = createPost(1L, mockMember);
+        CommentDto.Request commentRequest = new CommentDto.Request(1L, "댓글 내용");
+        Comment comment = createComment(1L, mockMember, post.getId());
+
+        when(memberService.findAuthenticatedMember()).thenReturn(mockMember);
+        when(postRepository.findById(1L)).thenReturn(Optional.of(post));
+        when(commentRepository.save(any(Comment.class))).thenReturn(comment);
+
+        CommentDto.Response response = commentService.addCommentToPost(post.getId(), commentRequest);
+
+        assertEquals(comment.getId(), response.getId());
+        assertEquals(commentRequest.getComment(), response.getComment());
+        assertEquals(mockMember.getNickname(), response.getAuthor());
+        assertEquals(post.getId(), comment.getPostId());
+
+        verify(commentRepository, times(1)).save(any(Comment.class));
     }
 
     @Test
